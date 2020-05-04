b7de23fa3a1f88cf4c031d5670627f53b70d4c9b
Roberto Rodriguez <rrodri0622@gmail.com>
2020-05-04 18:54:35 -0400

    Updated Agenda and links

---
+++ scripts/create_talks.py
@@ -0,0 +1,23 @@
+from jinja2 import Template
+import copy
+import yaml
+import glob
+from os import path
+
+print("[+] Processing files inside {} directory".format('../talks'))
+# ******** Open every forge yaml file available ****************
+print("  [>] Opening forge yaml files..")
+yaml_files = glob.glob(path.join(path.dirname(__file__), '../talks', "*.yaml"))
+yaml_loaded = [yaml.safe_load(open(yf).read()) for yf in yaml_files]
+
+# ******** Open forge template ****************
+print("  [>] Reading template..")
+yaml_template = Template(open("templates/table_template.md").read())
+
+# Create Markdown file
+print("  [>] Writing docs to markdown ..")
+yaml_for_render = copy.deepcopy(yaml_loaded)
+
+# Generate the markdown
+markdown = yaml_template.render(renderyaml=yaml_for_render)
+open('../docs/content/agenda.md', 'w').write(markdown)
---
+++ scripts/templates/table_template.md
@@ -0,0 +1,7 @@
+# Agenda
+
+|Time|topic|
+| :---| :---|
+|10:00-10:15|Welcome<br>Roberto Rodriguez [@Cyb3rWard0g](http://twitter.com/@Cyb3rWard0g), Microsoft MSTIC|
+{% for talk in renderyaml|sort(attribute='time') %}|{{talk['time']}}|<b>{{talk['title']}}</b><br>{{talk['speaker']}}{% if talk['twitter'] %}{% set handle = talk['twitter'].split('@') %} [{{talk['twitter']}}](http://twitter.com/{{handle[1]}}){% endif %}, {{talk['company']}}<br><br><b>Description</b><br>{{talk['abstract']}}|
+{% endfor %}
