20ef3b5471fd1fd3f9fa88c35e39b50a2a3e52dd
Roberto Rodriguez <rrodri0622@gmail.com>
2020-05-04 22:40:00 -0400

    updated docs

---
+++ docs/_build/jupyter_execute/introduction.py
@@ -0,0 +1,81 @@
+# Infosec Jupyterthon!
+
+[![Open Source Love](https://badges.frapsoft.com/os/v3/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+
+![](images/infosec-jupyterthon-main.png)
+
+An open community event for security researchers to share their favorite notebooks with the infosec community. We meet virtually, share notebooks round robin style, and have fun learning more about Jupyter notebooks applied to the infosec field. A great place to meet other Infosec Jovyan!
+
+## Event Goals
+
+* Learn basic notebook concepts with the Infosec community!
+* Share knowledge around Jupyter notebooks used for real-world scenarios in the Infosec field!
+* Share information about how to integrate Jupyter notebooks with other open source projects in the Infosec community!
+* Have fun!
+
+## Agenda
+* [https://infosecjupyterthon.com/agenda.html](https://infosecjupyterthon.com/agenda.html)
+
+
+## Quick Information
+* **When?** May 8th, 2020
+* **Where?** [https://aka.ms/InfosecJupyterthonLive](https://aka.ms/InfosecJupyterthonLive)
+* **How Much?** Free
+
+## Registration
+* **Form** [https://bit.ly/InfosecJupyterthonRegistration](https://bit.ly/InfosecJupyterthonRegistration)
+
+## Call For Notebooks!
+If you are using Jupyter notebooks for any use case applied to Infosec, we want to hear from you! Presentation slides are not required. You and a notebook is enough!.
+
+* **CFN form:** https://bit.ly/InfosecJupyterthonCFN
+* **CFN starts:** 2020/04/20
+* **CFN ends:** 2020/05/01
+
+### A few submission guidelines
+* Well documented notebook with enough context (e.g. Markdown text) to tell the story behind the code.
+* A well defined step by step process to go from basic to advanced skills.
+* Use of open source code (e.g pandas) to allow attendees to replicate the research at home.
+* Notebook and data must be freely releasable
+* 15-30mins notebook presentation.
+
+## Open Infrastructure for an Open Event!
+
+Thanks to the [@mybinderteam](https://twitter.com/mybinderteam), **all the material that can be tested at home for this event** will be shared and run in their public BinderHub environment. This website was built with the awesome project [Jupyter Book](https://github.com/jupyter/jupyter-book) and it is already integrated with [Binder](https://mybinder.org/). Therefore, you could use this site to know about the event, but also use it during the event to follow most of the notebooks shared by the community! You will be able to run most of the notebooks interactively without installing anything in your computer.
+
+![](images/binderhub-process.png)
+
+
+## What is a Notebook?
+Think of a notebook as a document that you can access via a web interface that allows you to save input (i.e. live code) and output (i.e. code execution results / evaluated code output) of interactive sessions as well as important notes needed to explain the methodology and steps taken to perform specific tasks (i.e data analysis).
+
+## What is a Jupyter Notebook?
+> The Jupyter Notebook is an open-source web application that allows you to create and share documents that contain live code, equations, visualizations and narrative text. Uses include: data cleaning and transformation, numerical simulation, statistical modeling, data visualization, machine learning, and much more.
+
+## What is a Jovyan?
+
+You may see the word Jovyan used in Jupyter tools (such as the user ID in the Jupyter Docker stacks or referenced in conversations. But what is a Jovyan?
+
+In astronomical terms, the word “Jovian” means “like Jupiter”. It describes several planets that share Jupyter-like properties.
+
+Much like the planet Jupiter and our solar system, the Jupyter community is large, distributed, and nebulous. We like to use the word Jovyan to describe members of this community. Jovyans are fellow open enthusiasts that use, develop, promote, teach, learn, and otherwise enjoy tools in Jupyter’s orbit. They make up the Jupyter community. If you’re not sure whether you’re a Jovyan, you probably are :-)
+
+## References
+
+* [https://mybinder.readthedocs.io/en/latest/](https://mybinder.readthedocs.io/en/latest/)
+* [https://medium.com/threat-hunters-forge/writing-an-interactive-book-over-the-threat-hunter-playbook-with-the-help-of-the-jupyter-book-3ff37a3123c7](https://medium.com/threat-hunters-forge/writing-an-interactive-book-over-the-threat-hunter-playbook-with-the-help-of-the-jupyter-book-3ff37a3123c7)
+* [https://medium.com/threat-hunters-forge/threat-hunter-playbook-mordor-datasets-binderhub-open-infrastructure-for-open-8c8aee3d8b4](https://medium.com/threat-hunters-forge/threat-hunter-playbook-mordor-datasets-binderhub-open-infrastructure-for-open-8c8aee3d8b4)
+* [https://jupyter.readthedocs.io/en/latest/community/content-community.html#what-is-a-jovyan](https://jupyter.readthedocs.io/en/latest/community/content-community.html#what-is-a-jovyan)
+* [https://discover-cookbook.numfocus.org/](https://discover-cookbook.numfocus.org/)
+
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: Infosec Jupyterthon
+
+content/agenda
+```
